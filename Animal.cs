1d387613020bb22a4d354ceb3de2fe3449a59c4a
camiglz <cami.glz30@gmail.com>
2026-04-23 14:26:11 +0000

    generación de todo el código

--- Animal.cs
+++ Animal.cs
@@ -1,1 +1,30 @@
+//Camila González Mota
+//Creación de clase animal
+using System;
 
+class Animal 
+{
+   //Atributos 
+    public string Nombre;
+    public int Edad; 
+
+    //Constructor
+    public Animal (string nombre, int edad)
+    {
+        //Parametros 
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    //Método
+    public void HacerSonido()
+    {
+        Console.WriteLine (Nombre + " hace un sonido. ");
+    }
+
+    //Método para mostar info
+    public void MostrarInfo()
+    {
+        Console.WriteLine ("Nombre: " + Nombre + ", Edad: " + Edad);
+    }
+}
